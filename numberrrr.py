fe57131f24eb5971cc4c819d402a7cc4622ead4b
mj-mi-1389 <161434945+mj-mi-1389@users.noreply.github.com>
2024-02-27 12:26:36 +0330

    Add files via upload

---
+++ numberrrr.py
@@ -0,0 +1,12 @@
+def number(num,num2):
+    print(num**num2)
+def number2(num3,num4):
+    print(num3*num4)
+def number3(num5,num6):
+    print(num5/num6)
+def number4(num7,num8):
+    print(num7//num8)
+def number5(num9,num10):
+    print(num9+num10)
+def number6(num11,num12):
+    print(num11-num12)
